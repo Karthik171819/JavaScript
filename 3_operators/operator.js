d17937bff3d705fcd6f2004feb9dc7c3624f989c
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-17 21:00:44 +0530

    operators

---
+++ 3_operators/operator.js
@@ -0,0 +1,11 @@
+//Operators
+let a = 5;
+let b = 3;
+
+console.log("a =", a, "& b= ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b); //Modulus
+console.log("a ** b = ", a**b); //Exponent
