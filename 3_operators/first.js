d882124a914980b5f680f3725aefa780e010513c
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-17 20:42:29 +0530

    comments

--- 3_operators/first.js
+++ 3_operators/first.js
@@ -1,2 +1,5 @@
  // This a single comment - code prints hello world
 console.log("Hello World!"); 
+
+/* This is a multi-line comment used to descriptive the code
+globally through understand or refering code actually what does it */
