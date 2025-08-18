7b51799964cecc77afd51648c1c7e04181dda181
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-18 22:52:25 +0530

    assignment operators

---
+++ 3_operators/assignment.js
@@ -0,0 +1,24 @@
+//Assignment operator
+
+let a = 5;
+let b = 2;
+
+// +=
+a+=4;
+console.log("a+= ",a);
+
+// -=
+a-=4;
+console.log("a-= ",a);
+
+// *=
+a*=4;
+console.log("a+= ",a);
+
+// %=
+b%=4;
+console.log("b/= ",b);
+
+// **=
+b **=4;
+console.log("b** = ", b);
