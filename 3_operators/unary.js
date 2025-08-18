986cbc6646c7c37ffd45078f8c11ad70e2a93a98
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-18 22:36:08 +0530

    unary operators

---
+++ 3_operators/unary.js
@@ -0,0 +1,19 @@
+//unary operator
+let a = 5;
+let b = 2;
+
+//Post increment
+a++
+console.log("a++ = ", a); 
+
+//Pre-Increment
+++a
+console.log("++a = ", a);
+
+//Pre - decrement
+--a
+console.log("--a = ", --a);
+
+//Post - Decrement
+console.log("a++ =", a++);
+console.log(a)
