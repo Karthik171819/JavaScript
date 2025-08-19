8cd3542e8689fbe5fd53f18996de57ea44abb98d
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-19 13:23:26 +0530

    comparison

--- 3_operators/comparison.js
+++ 3_operators/comparison.js
@@ -4,3 +4,11 @@ let b = 2;
 
 console.log(a == b); //false
 console.log(a == a); //true
+console.log(a != b); //true
+console.log(a === a);
+console.log(a === b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a !== a);
