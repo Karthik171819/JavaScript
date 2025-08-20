ced6e7f4de77ae0b655868bcc6d2ad530ebf1e9f
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-20 20:54:56 +0530

    string fucntion

---
+++ 5_loops_Strings/StringMethod.js
@@ -0,0 +1,23 @@
+//string_methods
+let str = "Hi Java Script";
+console.log(str); //OG String, immutable means cant change the original string 
+
+//UpperCase
+console.log(str,str.toUpperCase(str));
+
+// LowerCase
+console.log(str,str.toLowerCase(str));
+
+//trim white space
+console.log(str,str.trim());
+
+//string slicing
+console.log(str.slice(3, 8));
+
+//string concatenate
+{
+    let str1 = "KAR";
+    let str2 = "THIK";
+
+    console.log(str1.concat(str2));
+}
