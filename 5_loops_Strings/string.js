43ad299f905e1f24865b726238ba78e8bffb226e
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-20 20:18:39 +0530

    strings

---
+++ 5_loops_Strings/string.js
@@ -0,0 +1,22 @@
+//string
+let str = "string";
+console.log(str);
+
+//finding string length
+console.log(str.length);
+
+//strinding indexing
+console.log(str[0]);
+
+//Template literal
+let literal = `This is a Template Literal`;
+console.log(typeof(literal));
+
+//why we use string interpolation live example
+let obj = {
+    item : "Laptop",
+    price : 50000,
+}
+console.log(obj.item, obj.price); //it is a raw method
+let strinterpolation = `The cost of ${obj.item} is ${obj.price}`; //here price is not highlted it converts a whole string
+console.log(strinterpolation);
