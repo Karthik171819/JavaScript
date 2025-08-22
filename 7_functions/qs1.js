b667cf9940cb2d4feb3d8abc333a7fd5b0dc43f5
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-22 19:12:16 +0530

    practice question

--- 7_functions/qs1.js
+++ 7_functions/qs1.js
@@ -1,1 +1,13 @@
 //create a function and take string argument and returns the number of vowels in a string
+ let count = 0;
+function vowels(str){
+    for(let char of str){
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u" 
+        || char === "A" || char === "E" || char === "I" || char === "O" || char === "U" ){
+            count ++;
+        }
+    }
+    return count;
+}
+let result = vowels("Iron man aA eu bye");
+console.log(result);
