894c65d53e34b46ea6cb479a24022b059dbc9cbc
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-22 19:20:56 +0530

    practice arrow & norm function

--- 7_functions/qs1.js
+++ 7_functions/qs1.js
@@ -11,3 +11,20 @@ function vowels(str){
 }
 let result = vowels("Iron man aA eu bye");
 console.log(result);
+
+//same use arrow function
+{
+let vowels = (str) => {
+     let count = 0;
+    for(let char of str){
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u" 
+        || char === "A" || char === "E" || char === "I" || char === "O" || char === "U" ){
+            count ++;
+        }
+    }
+    return count;
+}
+let result = vowels("Iron man aA eu bye");
+console.log(result);
+
+}
