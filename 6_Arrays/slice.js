0b1887e547018e9727b148c478aa4a841c6645b1
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-22 12:15:48 +0530

    array slice

---
+++ 6_Arrays/slice.js
@@ -0,0 +1,6 @@
+//slice method it is inclusive the starting index 
+//slice(startidx, endidx);
+let hero = ["vijay", "allu arjun", "prabhas", "rajini"];
+let sep = hero.slice(1, 3);
+console.log(sep);
+console.log(hero);
