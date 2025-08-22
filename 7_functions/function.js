307afa0dcab5bf6f295c0b0489fd7f85491c15d3
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-22 18:28:22 +0530

    function

--- 7_functions/function.js
+++ 7_functions/function.js
@@ -3,3 +3,9 @@ function greeting(){
     console.log("Namaskram");
 }
 greeting();
+
+//parameterized function
+function para(str, n){
+    console.log(str, n);
+}
+para("parameter", 5);
---
+++ 7_functions/function1.js
@@ -0,0 +1,8 @@
+//add two number using function
+function add(num1, num2){
+    let res = num1+num2;
+  //console.log(res);
+    return res;
+}
+ let fresult = add(5, 3);
+ console.log(fresult);
