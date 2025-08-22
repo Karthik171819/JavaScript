e5f4d8d52536b37049ad08e3ff39b2178aa74170
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-22 12:29:05 +0530

    array splicing

---
+++ 6_Arrays/splice.js
@@ -0,0 +1,14 @@
+//array splice it can change the original array and add, remove, update
+//splice(startidx, delcountel, newel);
+
+let arr = [1, 2, 3, 4, 5, 6];
+arr.splice(6,0,0);
+console.log(arr);
+
+//adding new element
+arr.splice(2, 1, 2.1);
+console.log(arr);
+
+//delete element
+arr.splice(3,1);
+console.log(arr);
