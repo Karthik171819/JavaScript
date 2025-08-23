05b680afc4ccd9fb700c7fa93c246061a2441805
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-23 12:56:24 +0530

    map

---
+++ 7_functions/map.js
@@ -0,0 +1,7 @@
+//map it is also a similar to forEach but difference is here we store a result in new array
+let arr = [1,2,3,4];
+let newArr = arr.map((value) => {
+    return value * 2;
+});
+console.log(newArr);
+
