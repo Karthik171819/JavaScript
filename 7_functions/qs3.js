2702a769b1951f36d23aa6dec07fcc3879b2b6af
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-23 14:31:21 +0530

    practice

---
+++ 7_functions/qs3.js
@@ -0,0 +1,12 @@
+//create an array  calculate the sum of an array and product an array elements
+var arr = [2, 3, 4, 5, 6, 7];
+var result = arr.reduce((prev_el, curr_el) =>{
+    return prev_el + curr_el ;
+});
+console.log(result);
+
+var arr = [2, 3, 4, 5, 6, 7];
+var result = arr.reduce((prev_el, curr_el) =>{
+    return prev_el * curr_el ;
+});
+console.log(result);
