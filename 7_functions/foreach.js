ad55475a859a97e55cb4728d92ba3e6e1c8ab319
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-23 12:37:30 +0530

    forEach

---
+++ 7_functions/foreach.js
@@ -0,0 +1,12 @@
+var arr = ["pune", "banglore", "chennai", "mumbai"];
+
+arr.forEach((val)=>{
+    console.log(val);
+});
+
+{
+    var arr = [1, 2, 3, 4, 5];
+    arr.forEach((val) => {
+        console.log(val);
+    });
+}
---
+++ 7_functions/foreach1.js
@@ -0,0 +1,7 @@
+//given array print square of each value
+
+let arr = [ 1, 2, 3, 4, 5];
+
+arr.forEach((val) => {
+    console.log(val * val);
+} );
