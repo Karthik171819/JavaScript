11bb8b0881d817ffc70fc89a80df5bdad4dbbe2e
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-23 14:05:10 +0530

    reduce

--- 7_functions/reduce.js
+++ 7_functions/reduce.js
@@ -1,22 +1,21 @@
 //reduce it is used to return the single line of value
 let arr = [1, 2, 3, 4];
-
 let resultArr = arr.reduce((prev_el, curr_el) => {
     return prev_el + curr_el;
 });
 console.log(`original array [${arr}] sum of an array = ${resultArr}`);
 
+
 //finding the maximum array element
 var arrElements = [ 12, 34, 56, 98, 56, 45, 200];
-
 var result = arrElements.reduce((a, b) =>{
     return a > b ? a : b;
 });
 console.log(result);
 
+
 //finding the minimum array element
 var arrElements = [ 12, 34, 56, 98, 56, 45, 200];
-
 var result = arrElements.reduce((a, b) =>{
     return a < b ? a : b;
 });
