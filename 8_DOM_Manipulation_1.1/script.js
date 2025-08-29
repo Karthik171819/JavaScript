0a41438de2825063593f062117f2224d65614052
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-29 20:06:06 +0530

    id & className

--- 8_DOM_Manipulation_1.1/script.js
+++ 8_DOM_Manipulation_1.1/script.js
@@ -6,3 +6,7 @@ console.log(heading);
 // accessing get elements by classname
 var className = document.getElementsByClassName('header-class');
 console.log(className);
+
+// accesiing  get elemnets by TagName
+var para = document.getElementsByTagName("p");
+console.log(para);
