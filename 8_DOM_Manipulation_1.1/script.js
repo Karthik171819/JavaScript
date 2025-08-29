11f5d540e4b32aef2b578188d8f3bd653d235a02
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-29 19:52:57 +0530

    id & className

--- 8_DOM_Manipulation_1.1/script.js
+++ 8_DOM_Manipulation_1.1/script.js
@@ -1,2 +1,8 @@
 // alert("script works well");
 document.body.style.background = "orange";
+var heading = document.getElementById("heading");
+console.log(heading);
+
+// accessing get elements by classname
+var className = document.getElementsByClassName('header-class');
+console.log(className);
