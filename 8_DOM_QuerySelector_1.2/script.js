d1a2241e960377c1eff96802bbbdd5437153a9fd
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-31 13:10:02 +0530

    QuerySelector

--- 8_DOM_QuerySelector_1.2/script.js
+++ 8_DOM_QuerySelector_1.2/script.js
@@ -1,2 +1,7 @@
-let firstel = document.querySelector("p");
-    console.dir(firstel);
+let firstel = document.querySelector("#btn"); // only the first elements
+console.dir(firstel);
+firstel.tagName; //TagName
+
+// let allel = document.querySelectorAll("p"); //all elements print in console
+// console.log(allel);
+
