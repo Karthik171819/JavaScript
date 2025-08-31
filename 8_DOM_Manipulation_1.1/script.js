ca1197fff367742d485257a12552a4626582dd72
Karthik171819 <karthikkumar092004@gmail.com>
2025-08-31 10:05:25 +0530

    DOM Manipulation

--- 8_DOM_Manipulation_1.1/script.js
+++ 8_DOM_Manipulation_1.1/script.js
@@ -1,9 +1,10 @@
 // alert("script works well");
 document.body.style.background = "orange";
-var heading = document.getElementById("heading");
+//id accessing #
+var heading = document.getElementById("heading"); 
 console.log(heading);
 
-// accessing get elements by classname
+// accessing get elements by classname (.)
 var className = document.getElementsByClassName('header-class');
 console.log(className);
 
