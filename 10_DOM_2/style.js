8c64b751266c5d77f3cf72b33604601b910991fa
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-01 13:41:12 +0530

    styling through Js

---
+++ 10_DOM_2/style.js
@@ -0,0 +1,13 @@
+//printing a div
+var div = document.querySelector("div");
+console.log(div);
+div.style; //write in console
+
+//fontsize changing
+div.style.fontSize="30px";
+
+//innertext changing
+div.innerText = "Hello Boss!";
+
+//suppose i need to hide my JS style 
+//div.style.visibility = "hidden";
