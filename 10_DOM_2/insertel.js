597101741b0a483b2d732450ec2da75719a9db3e
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-01 14:56:46 +0530

    inserting, deleting or crete new_el and with properties

--- 10_DOM_2/insertel.js
+++ 10_DOM_2/insertel.js
@@ -4,7 +4,7 @@ newbtn.innerText = "click me!";
 console.log(newbtn);
 
 //appending new element btn end of the node(inside)
-let div = document.querySelector("div");
+var div = document.querySelector("div");
 div.append(newbtn);
 
 //suppose i need to be btn at first (inside)
@@ -15,3 +15,15 @@ div.before(newbtn);
 
 //adds after the node (outside)
 div.after(newbtn);
+
+//creating new heading
+let newheading = document.createElement("h5")
+newheading.innerHTML = "<i> Hi iam New Heading </i>";
+console.log(newheading);
+
+var div = document.querySelector("div");
+div.before(newheading);
+
+//remove the paragraph 
+let para = document.querySelector("p");
+para.remove(); //para sucessfully removed
