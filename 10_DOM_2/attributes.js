0553d5919dc20595b7b816027b99b78a4d390efc
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-01 12:47:14 +0530

    DOM_getAttributes

---
+++ 10_DOM_2/attributes.js
@@ -0,0 +1,13 @@
+var div = document.querySelector("div");
+console.log(div);
+
+var id = div.getAttribute("id");
+console.log(id);
+
+//getting name attribute
+var name_att = div.getAttribute("name");
+console.log(name_att);
+
+//getting para class name
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
