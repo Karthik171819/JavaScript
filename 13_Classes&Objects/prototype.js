2b8f678445b2ba064e46df9e4fccf5351fcc5a40
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-05 15:01:51 +0530

    prototype

---
+++ 13_Classes&Objects/prototype.js
@@ -0,0 +1,14 @@
+const employe = {
+    calcTax() {
+        console.log("toatal tax discount 10%")
+    },
+};
+
+const john = {
+    salary: 50000,
+    calcTax() {
+        console.log("total tax 20%");
+    }
+};
+
+john.__proto__ = employe;
