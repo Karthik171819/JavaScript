06951dca853acb29bfd4d73331ae828c5728d1d9
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-10 16:08:23 +0530

    classes and objects

--- 13.1_Classes/classes.js
+++ 13.1_Classes/classes.js
@@ -1,3 +1,4 @@
+//why we use class code template means we can use multiple objects from single class
 class Car {
     start() {
         console.log("car started");
@@ -11,8 +12,16 @@ class Car {
         this.brand = brand; //here this.brand and brand is different varaiable this.brand is from class property anotherone is function argument
     }
 }
+//object 1
 let fortuner = new Car();
 fortuner.setBrand("fortuner");
 console.log(fortuner);
 fortuner.start();
 fortuner.stop();
+
+//object 2
+let innova = new Car();
+innova.setBrand("Innova");
+console.log(innova);
+innova.start();
+innova.stop();
