7adc6433a9d0db155d9d5a4caa3445989730b6a7
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-10 20:27:15 +0530

    constructor

--- 13.1_Classes/constructor.js
+++ 13.1_Classes/constructor.js
@@ -1,8 +1,9 @@
-//calling constructor
+//calling constructor, constructor ini its automatically invoked or called 
 class Car {
-    constructor(brand){
+    constructor(brand, mileage){
         console.log("Constructor called");
         this.brand = brand;
+        this.mileage = mileage;
     }
 
     start(){
@@ -10,6 +11,7 @@ class Car {
     }
 }
 
-let fortuner = new Car("Toyato");
-console.log(fortuner.brand);
+let fortuner = new Car("Toyato", 17); //actually here mileage is 17
+fortuner.mileage = 15; //but here im changing to 15
+console.log(fortuner);
 fortuner.start();
