0e5cdb227203028b319c05a4f708b89742f7dc41
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-10 15:57:26 +0530

    classes

---
+++ 13.1_Classes/classes.js
@@ -0,0 +1,18 @@
+class Car {
+    start() {
+        console.log("car started");
+    }
+
+    stop() {
+        console.log("car stopped");
+    }
+
+    setBrand(brand) {
+        this.brandName = this.brand;
+    }
+}
+let fortuner = new Car();
+fortuner.setBrand("fortuner");
+console.log(fortuner);
+fortuner.start();
+fortuner.stop();
