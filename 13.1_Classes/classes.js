47c484c2e13d65a996cd12795b4dea7347819f28
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-10 16:02:24 +0530

    classes and objects

--- 13.1_Classes/classes.js
+++ 13.1_Classes/classes.js
@@ -8,7 +8,7 @@ class Car {
     }
 
     setBrand(brand) {
-        this.brand = brand; 
+        this.brand = brand; //here this.brand and brand is different varaiable this.brand is from class property anotherone is function argument
     }
 }
 let fortuner = new Car();
