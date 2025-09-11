ae32facf83722139ad7076812a9640ca2eafd218
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-11 21:03:23 +0530

    super

---
+++ 13.1_Classes/super.js
@@ -0,0 +1,25 @@
+//super keyword is used to call the constructor of its parent class to access the parents properties and methods
+class Person {
+    constructor() {
+        console.log("entered parent constructor");
+        this.species = "Homo phones";
+    }
+    eat() {
+        console.log("eating");
+    }
+}
+
+class Engineer extends Person {
+    constructor(branch){
+        console.log("entered child constructor");
+        super(); //invoke the parent constructor
+        console.log("exit child constructor");
+        this.branch = branch;
+    }
+    work() {
+        console.log("Experienced engnieeer ");
+    }
+}
+
+let engObj = new Engineer("CS Engineer");
+console.log(engObj);
