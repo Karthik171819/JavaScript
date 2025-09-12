f6ccbd2b66e986cb4ccd89eef34023ea11a9c1a7
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-12 23:18:15 +0530

    synchronization

--- 15_Async/async.js
+++ 15_Async/async.js
@@ -1,4 +1,4 @@
-//synchronization 
+//synchronization - Executes the instruction one by one actually the next instruction execution  is depending on previous one
 console.log("first sequence");
 console.log("second sequence");
 console.log("third sequence");
