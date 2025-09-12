9afeb8ae1d2b43443c1ae55a5240f07f7199a43a
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-12 14:52:15 +0530

    try-catch

---
+++ 14_ErrorHandling/error.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 5;
+
+console.log(a+b);
+try{
+    console.log(a*c);
+} catch(err){           //err is the error object
+    console.log(err);
+};
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
