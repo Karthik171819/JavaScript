75ce64df13978f6ef5d0b01333da2c2dab94fc33
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-12 23:22:42 +0530

    synchronization

--- 15_Async/sync.js
+++ 15_Async/sync.js
@@ -3,3 +3,8 @@ console.log("first sequence");
 console.log("second sequence");
 console.log("third sequence");
 console.log("fourth sequence");
+
+function hello() {
+    console.log("hello");
+}
+setTimeout(hello, 4000) //4000ms is equal to 4s
