bfc981351185cf8170d6dba8427060d28113325f
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-13 11:58:09 +0530

    Callback Hell

---
+++ 16_Callback/callbackHell.js
@@ -0,0 +1,11 @@
+//nesting 
+var age = 19;
+if(age >= 18){
+    if(age >= 60 ){
+        console.log("senior");
+    } else {
+        console.log("middle age person");
+    }
+} else {
+    console.log("child");
+}
