59b0815e13bcd8778156ae9d0a2512328eb446f4
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-14 17:38:15 +0530

    Async-await

--- 15_Async/async-await1.js
+++ 15_Async/async-await1.js
@@ -9,8 +9,13 @@ function getdata(dataId){
 
 //Async-Await
 async function fetch(){
+    console.log("getting data 1...");
     await getdata(1);
+
+    console.log("getting data 2...");
     await getdata(2);
+
+    console.log("getting data 3...");
     await getdata(3);
 };
 
