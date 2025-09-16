4d7a01743f3c2fe8eddf58905e7405d9d1173d83
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-16 18:23:56 +0530

    await

--- 15_Async/await.js
+++ 15_Async/await.js
@@ -3,7 +3,7 @@ function api(){
     return new Promise((resolve, reject)  => {
         setTimeout(() => {
             console.log("weather data");
-        resolve(200);
+        resolve("success");
         }, 2000);
     });
 }
