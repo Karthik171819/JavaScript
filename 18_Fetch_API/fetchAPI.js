29343ea01572c356d18fd132def84a9e84798d9f
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-16 20:35:31 +0530

    fetch API

--- 18_Fetch_API/fetchAPI.js
+++ 18_Fetch_API/fetchAPI.js
@@ -1,8 +1,16 @@
 const URL = "https://api.thecatapi.com/v1/images/0XYvRd7oD";
+const factPara = document.querySelector('#fact');
+const btn = document.querySelector('#btn');
 
 //lets call api using async- await function
 let getData = async () => {
     console.log("gettig data please wait...")
-    let response = await fetch(URL);
+    var response = await fetch(URL);
     console.log(response);
+
+    //json format
+    var data = await response.json();
+    console.log(data);
 };
+
+btn.addEventListener("click", getData);
