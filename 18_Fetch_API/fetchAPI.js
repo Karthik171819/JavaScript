20466cd095ca698d95b39129aefbe9292fd3358e
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-16 20:07:15 +0530

    fetch API

--- 18_Fetch_API/fetchAPI.js
+++ 18_Fetch_API/fetchAPI.js
@@ -2,6 +2,7 @@ const URL = "https://api.thecatapi.com/v1/images/0XYvRd7oD";
 
 //lets call api using async- await function
 let getData = async () => {
+    console.log("gettig data please wait...")
     let response = await fetch(URL);
     console.log(response);
 };
